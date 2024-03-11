4dabe708a101428e017f707f9dee8bae24f9f99a
tjoluotch <trevorjoluotch@gmail.com>
2024-03-11 15:49:38 +0000

    added test for error handling on scan items

--- tests/checkout_test.go
+++ tests/checkout_test.go
@@ -2,14 +2,17 @@ package tests
 
 import (
 	"checkout-kata/pkg"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+var ()
+
 func TestCheckout(t *testing.T) {
 
-	t.Run("Scan Item", func(t *testing.T) {
+	t.Run("scan Item", func(t *testing.T) {
 		checkout := pkg.NewShopper()
-		checkout.ScanItem("A")
+		_ = checkout.ScanItem("A")
 
 		sh := getConcreteTypeShopper(t, checkout)
 
@@ -18,9 +21,20 @@ func TestCheckout(t *testing.T) {
 		}
 	})
 
+	t.Run("scan item that isn't in price list, error expected", func(t *testing.T) {
+		scannedItem := "A33"
+		checkout := pkg.NewShopper()
+		err := checkout.ScanItem(scannedItem)
+
+		// if error returned is nil
+		if !assert.NotNil(t, err) {
+			t.Errorf("expected error as SKU-%s isn't in price list", scannedItem)
+		}
+	})
+
 	t.Run("scan item and get correct total", func(t *testing.T) {
 		checkout := pkg.NewShopper()
-		checkout.ScanItem("B")
+		_ = checkout.ScanItem("B")
 
 		sh := getConcreteTypeShopper(t, checkout)
 
@@ -35,9 +49,6 @@ func TestCheckout(t *testing.T) {
 		}
 	})
 
-	t.Run("test adding single item to checkout", func(t *testing.T) {
-		//s := new(pkg.Shopper)
-	})
 }
 
 func getConcreteTypeShopper(t testing.TB, checkout pkg.CheckoutIntf) (concrete *pkg.Shopper) {
