dcb9f15a4d98fab1a064e9cb277ef11f262c3e7c
tjoluotch <trevorjoluotch@gmail.com>
2024-03-11 13:09:37 +0000

    added test implementation for scannig item

--- tests/checkout_test.go
+++ tests/checkout_test.go
@@ -1,9 +1,22 @@
 package tests
 
-import "testing"
+import (
+	"checkout-kata/pkg"
+	"testing"
+)
 
 func TestCheckout(t *testing.T) {
-	t.Run("test adding single item to checkout", func(t *testing.T) {
 
+	t.Run("Scan Item", func(t *testing.T) {
+		sh := pkg.NewShopper()
+		sh.ScanItem("A")
+
+		if len(sh.Store) != 1 {
+			t.Error("failed to scan item")
+		}
+	})
+
+	t.Run("test adding single item to checkout", func(t *testing.T) {
+		//s := new(pkg.Shopper)
 	})
 }
