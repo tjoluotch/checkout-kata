7190a870cc218f25605100a50a9b48cc6082c180
tjoluotch <trevorjoluotch@gmail.com>
2024-03-11 13:01:21 +0000

    defined outline of checkout system and created shopper composite

--- pkg/items.go
+++ pkg/items.go
@@ -1,9 +1,32 @@
 package pkg
 
+import "sync"
+
 type ItemDetails struct {
-	Name         string
-	UnitPrice    float64
+	SKU          string
+	UnitPrice    int64
 	SpecialOffer Offer
+	Quantity     int
 }
 
-type Offer map[int]float64
+type Offer map[int]int64
+
+type Store []ItemDetails
+
+type CheckoutIntf interface {
+	ScanItem(sku string)
+	GetTotal() int64
+}
+
+type Shopper struct {
+	Store []Store
+	mu    sync.Mutex
+}
+
+func (s *Shopper) ScanItem(sku string) {
+
+}
+
+func (s *Shopper) GetTotal() int64 {
+	return 0
+}
