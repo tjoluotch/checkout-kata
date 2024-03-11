dadd4d68e49dcdb0472e980e8314b6f014276a44
tjoluotch <trevorjoluotch@gmail.com>
2024-03-11 13:42:21 +0000

    defined test helper to get concrete type underlying checkout and perform test checks

--- tests/checkout_test.go
+++ tests/checkout_test.go
@@ -8,8 +8,10 @@ import (
 func TestCheckout(t *testing.T) {
 
 	t.Run("Scan Item", func(t *testing.T) {
-		sh := pkg.NewShopper()
-		sh.ScanItem("A")
+		checkout := pkg.NewShopper()
+		checkout.ScanItem("A")
+
+		sh := getConcreteTypeShopper(t, checkout)
 
 		if len(sh.Store) != 1 {
 			t.Error("failed to scan item")
@@ -19,4 +21,14 @@ func TestCheckout(t *testing.T) {
 	t.Run("test adding single item to checkout", func(t *testing.T) {
 		//s := new(pkg.Shopper)
 	})
+}
+
+func getConcreteTypeShopper(t testing.TB, checkout pkg.CheckoutIntf) (concrete *pkg.Shopper) {
+	t.Helper()
+	concrete, ok := checkout.(*pkg.Shopper)
+	if !ok {
+		t.Error("failed to get concrete type of checkout")
+		return nil
+	}
+	return
 }
