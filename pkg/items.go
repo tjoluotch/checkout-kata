b2fe928f3500817f192441af029b354ca3f22562
tjoluotch <trevorjoluotch@gmail.com>
2024-03-11 15:15:15 +0000

    added implmentation of getTotal and refactored scanItem

--- pkg/items.go
+++ pkg/items.go
@@ -1,12 +1,16 @@
 package pkg
 
-import "sync"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
 type ItemDetails struct {
 	SKU          string
 	UnitPrice    int64
 	SpecialOffer Offer
-	Quantity     int
+	//Quantity     int
 }
 
 type Offer map[int]int64
@@ -16,7 +20,7 @@ type Store []ItemDetails
 type PriceList map[string]int64
 
 type CheckoutIntf interface {
-	ScanItem(sku string)
+	ScanItem(sku string) error
 	GetTotal() int64
 }
 
@@ -34,14 +38,43 @@ func NewShopper() CheckoutIntf {
 	}
 }
 
-func (s *Shopper) ScanItem(sku string) {
+func (s *Shopper) ScanItem(sku string) error {
+	// check sku is in priceList
+	var found string
+	for key, _ := range s.PList {
+		if key != sku {
+			continue
+		} else {
+			found = sku
+			break
+		}
+	}
+	// error reporting if sku not found in price list
+	if found == "" {
+		errors.New(fmt.Sprintf("the given SKU %s is not in the store's price list, try again", sku))
+	}
+
+	// get unit price of sku
+	price := s.PList[found]
+
 	s.mu.Lock()
-	s.Store = append(s.Store, ItemDetails{SKU: sku})
+	s.Store = append(s.Store, ItemDetails{SKU: found, UnitPrice: price})
 	s.mu.Unlock()
+
+	return nil
 }
 
 func (s *Shopper) GetTotal() int64 {
-	return 0
+	total := int64(0)
+	// if store has no items
+	if len(s.Store) < 1 {
+		return 0
+	}
+
+	for _, item := range s.Store {
+		total += item.UnitPrice
+	}
+	return total
 }
 
 func getPriceList() PriceList {
